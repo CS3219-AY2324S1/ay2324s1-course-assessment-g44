9da1f4a80cfd5917808c056a8197cf110f69f587
Justin Cheng <justcheng2000@gmail.com>
2023-09-09 22:29:20 +0800

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
-import Create from './components/create';
-import Read from './components/read';
-import Update from './components/update';
-import { BrowserRouter as Router,Route } from 'react-router-dom'
+import "./App.css";
+import Create from "./components/create";
+import Read from "./components/read";
+import Update from "./components/update";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <div className="main">
-        <Router>
-          <h2 className="main-header">Peer Prep (Team 44)</h2>
-          <div>
-            <Route exact path='/create' component={Create} />
-          </div>
-          <div style={{ marginTop: 20 }}>
-            <Route exact path='/read' component={Read} />
-          </div>
-            <Route path='/update' component={Update} />
-        </Router>
-      </div>
+    <div className="main">
+      <Router>
+        <h2 className="main-header">Peer Prep (Team 44)</h2>
+        <div>
+          <Routes>
+            <Route path="/create" element={<Create/>} />
+          </Routes>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Routes>
+            <Route path="/read" element={<Read/>} />
+          </Routes>
+        </div>
+        <Routes>
+          <Route path="/update" element={<Update/>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 

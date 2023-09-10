2fa4a9bf47da5263158f8d339894ff24365cd300
Justin Cheng <justcheng2000@gmail.com>
2023-09-10 15:12:45 +0800

    remove gitignore files

--- src/components/create.js
+++ src/components/create.js
@@ -1,41 +1,58 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'semantic-ui-react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export default function Create() {
-    const [questionName, setQuestionName] = useState('');
-    const [question, setQuestion] = useState('');
-    const [difficultyLevel, setDifficultyLevel] = useState('');
-    const navigate = useNavigate();
+  const [questionName, setQuestionName] = useState("");
+  const [question, setQuestion] = useState("");
+  const [difficultyLevel, setDifficultyLevel] = useState("");
+  const navigate = useNavigate();
 
-
-    const postData = () => {
-        axios.post(`https://64fc0579605a026163ae2051.mockapi.io/fakeData`, {
-            questionName,
-            question,
-            difficultyLevel
-        })
-
-        navigate('/read')
-    }
-    return (
-        <div>
-            <Form className="create-form">
-                <Form.Field>
-                    <label>Question Name</label>
-                    <input placeholder='Question Name' onChange={(e) => setQuestionName(e.target.value)}/>
-                </Form.Field>
-                <Form.Field>
-                    <label>Question</label>
-                    <input className='question-field' placeholder='Question' onChange={(e) => setQuestion(e.target.value)}/>
-                </Form.Field>
-                <Form.Field>
-                <label>Difficulty Level</label>
-                    <input placeholder='Difficulty Level' onChange={(e) => setDifficultyLevel(e.target.value)}/>
-                </Form.Field>
-                <Button onClick={postData} type='submit'>Add Question</Button>
-            </Form>
-        </div>
-    )
+  const postData = () => {
+    axios
+      .post(`https://64fc0579605a026163ae2051.mockapi.io/fakeData`, {
+        questionName,
+        question,
+        difficultyLevel,
+      })
+      .then(() => {
+        window.location.reload(true);
+      });
+  };
+  return (
+    <div>
+      <Form className="create-form">
+        <Form.Field className="question-name-field">
+          <label>Question Name</label>
+          <input
+            placeholder="Question Name"
+            onChange={(e) => setQuestionName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field className="question-field">
+          <label>Question</label>
+          <input
+            placeholder="Question"
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Difficulty Level</label>
+          <input
+            placeholder="Difficulty Level"
+            onChange={(e) => setDifficultyLevel(e.target.value)}
+          />
+        </Form.Field>
+        <Button
+          className="post-question-button"
+          onClick={postData}
+          type="submit"
+        >
+          Add Question
+        </Button>
+      </Form>
+    </div>
+  );
 }

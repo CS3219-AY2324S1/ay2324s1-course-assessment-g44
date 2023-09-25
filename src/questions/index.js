d27d1ec54c9793bd5334457879ba1d37e5cd8adc
Derrick Khoo <derrickkhoo2000@gmail.com>
2023-09-25 16:01:29 +0800

    Create question service

---
+++ src/questions/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const app = express();
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+const routes = require('./routes/routes');
+app.use('/api', routes)
---
+++ src/questions/models/question_model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    questionId: {
+        type: Number,
+        required: true
+    },
+
+    title: { 
+        type: String, 
+        required: true 
+    },
+
+    description: {
+        type: String, 
+        required: true 
+    },
+
+    category: { 
+        type: String,
+        required: true
+    },
+  
+    complexity: {
+        type: String,
+        required: true 
+    },
+
+});
+
+const questionModel = mongoose.model('Questions', questionSchema);  
+module.exports = questionModel;

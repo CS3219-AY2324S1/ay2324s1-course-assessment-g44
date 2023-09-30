27194a3d0575806e0c5c1333ad52f7d7009ffb5c
Derrick Khoo <derrickkhoo2000@gmail.com>
2023-09-30 15:33:00 +0800

    Add routing gateway for backend

--- src/questions/routes/questionRoutes.js
+++ src/questions/routes/questionRoutes.js
@@ -18,3 +18,21 @@
 // router.get("/getQuestions", questionController.getQuestions);
 
 // module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const questionController = require('../controllers/question_controller');
+
+// Post question
+router.post("/addQuestion", questionController.addQuestion);
+
+// Update question
+router.patch("/updateQuestion", questionController.updateQuestion);
+
+// Delete question
+router.delete("/deleteQuestion", questionController.deleteQuestion);
+
+// Get all questions
+router.get("/getQuestions", questionController.getQuestions);
+
+module.exports = router;

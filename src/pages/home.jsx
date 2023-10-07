2721a7a64d8b33a7b249e503a2595f8a965bb651
zylee348 <e0725436@u.nus.edu>
2023-10-07 11:44:00 +0800

    Add homepage elements

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header'
 import Navbar from '../components/navbar'
+import HomepageGrid from '../components/page_contents/homePageGrid'
 import { useDisclosure } from '@mantine/hooks';
 import { AppShell, Burger } from '@mantine/core';
 
@@ -23,7 +24,9 @@ function Home() {
             <Navbar/>
           </AppShell.Navbar>
     
-          <AppShell.Main>main home stuff here</AppShell.Main>
+          <AppShell.Main>
+            <HomepageGrid />
+          </AppShell.Main>
         </AppShell>
       );
     }

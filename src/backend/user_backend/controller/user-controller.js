554056fc5a0745a4d415af9034a1fdb8ea798f99
Sheryl Kong <sherylkong@Sheryls-MacBook-Air.local>
2023-10-19 14:03:16 +0800

    Update user db

--- src/backend/user_backend/controller/user-controller.js
+++ src/backend/user_backend/controller/user-controller.js
@@ -6,7 +6,6 @@ const {
   isCorrectPassword,
 } = require("../utils/validation");
 const jwt = require("jsonwebtoken");
-const {v4: uuidv4} = require("uuid");
 
 const EXPIRATION_TIME = 15 * 60; // 15 min
 const JWT_SECRET_KEY = "iloveJWT";
@@ -19,9 +18,8 @@ exports.createUser = async (req, res) => {
     if (isDuplicate) {
       return res.status(401).send();
     } else {
-      const newId = uuidv4();
       await pool.query(
-        `INSERT INTO Users VALUES ('${email}', '${username}', '${password}', '${newId}', '${ROLE}')`
+        `INSERT INTO Users(email_address, username, password, role) VALUES ('${email}', '${username}', '${password}', '${ROLE}')`
       );
       return res.status(201).send();
     }

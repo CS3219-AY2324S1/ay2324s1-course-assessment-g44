63c78b7822a2460ba9209bd4f191b7dac2d31c4d
Chustinjeng <justcheng2000@gmail.com>
2023-10-24 19:21:54 +0800

    Change login and sign up ui

---
+++ src/components/sign_up/signUpInfo.jsx
@@ -0,0 +1,72 @@
+import {
+  Container,
+  Paper,
+  Title,
+  Text,
+  Space,
+  List,
+  ThemeIcon,
+} from "@mantine/core";
+import classes from "../../css/Authentication.module.css";
+import { IconCircleCheck, IconFlame, IconTrophy, IconUsers } from "@tabler/icons-react";
+
+function SignUpInfo() {
+  return (
+    <Container size={440}>
+      <Paper className={classes.form} radius={0} p={30} shadow="md">
+        <Title ff="Verdana" fs={22} order={4}>
+          {" "}
+          Do you find yourself afraid of technical interviews?{" "}
+        </Title>
+        <Space h={30} />
+        <Text lh="xs">
+          Join us now! We are the world's leading platform in technical interview practice.
+        </Text>
+        <Space h={36} />
+        <List
+          spacing="lg"
+          size="sm"
+          center
+          icon={
+            <ThemeIcon color="teal" size={24} radius="xl">
+              <IconCircleCheck size="1rem" />
+            </ThemeIcon>
+          }
+        >
+          <List.Item>
+            Practice commonly-tested quetions in technical interviews
+          </List.Item>
+          <List.Item icon={
+            <ThemeIcon color="pink" size={24} radius="xl">
+                <IconUsers size="1rem" />
+            </ThemeIcon>
+          }
+          >
+            Connect with like-minded peers to discuss ideas
+          </List.Item>
+          <List.Item>Keep track of the questions you have completed</List.Item>
+          <List.Item
+            icon={
+              <ThemeIcon color="red" size={24} radius="xl">
+                <IconFlame size="1rem" />
+              </ThemeIcon>
+            }
+          >
+            Joining us is free!
+          </List.Item>
+          <List.Item
+            icon={
+              <ThemeIcon color="yellow" size={24} radius="xl">
+                <IconTrophy size="1rem" />
+              </ThemeIcon>
+            }
+          >
+            Succeed in your technical interviews!
+          </List.Item>
+        </List>
+      </Paper>
+    </Container>
+  );
+}
+
+export default SignUpInfo;

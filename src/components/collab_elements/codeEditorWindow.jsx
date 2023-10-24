6c5aed6f019593df5813c0330425f98d2a38dd2b
zylee348 <e0725436@u.nus.edu>
2023-10-24 22:12:12 +0800

    Fix bugs in code editor window

--- src/components/collab_elements/codeEditorWindow.jsx
+++ src/components/collab_elements/codeEditorWindow.jsx
@@ -6,52 +6,39 @@ import { MonacoBinding } from 'y-monaco';
 import * as monaco from 'monaco-editor';
 import { loader } from '@monaco-editor/react';
 
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-
-
 const CodeEditorWindow = ({roomID, language,code }) => {
   const [value, setValue] = useState(code || "");
 
   const editorRef = useRef(null); // Create a ref for the editor
 
-  useEffect(() => {
-    self.MonacoEnvironment = {
-      getWorker(_, language) {
-        if (language === 'java') {
-          return new javaWorker();
-        }
-        if (language === 'python') {
-          return new pythonWorker();
-        }
-        if (language === 'javascript') {
-          return new jsWorker();
-        }
-        if (language === 'typescript') {
-          return new tsWorker();
-        }
-        if (language === 'scala') {
-          return new scalaWorker();
-        }
-        if (language === 'ruby') {
-          return new rubyWorker();
-        }
-        if (language === 'go') {
-          return new goWorker();
-        }
-        if (language === 'c') {
-          return new cWorker();
-        }
-        if (language === 'c++') {
-          return new cppWorker();
-        }
-        return new editorWorker();
-      }
-    };
+  const handleEditorChange = (currContent) => {
+    if (!currContent) return;
+    setValue(currContent);
+  };
 
+  const options = {
+    autoIndent: "full",
+    contextmenu: true,
+    fontFamily: "monospace",
+    fontSize: 15,
+    lineHeight: 22,
+    hideCursorInOverviewRuler: false,
+    matchBrackets: "always",
+    minimap: {
+      enabled: true,
+    },
+    scrollbar: {
+      horizontalSliderSize: 4,
+      verticalSliderSize: 18,
+    },
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: "line",
+    automaticLayout: true,
+  };
+
+  const didEditorMount = (editor, monaco) => {
     // Y.js setup
     const ydoc = new Y.Doc();
     const provider = new WebsocketProvider("ws://localhost:1234", `${roomID}`, ydoc);
@@ -62,39 +49,26 @@ const CodeEditorWindow = ({roomID, language,code }) => {
       const editor = monaco.editor.create(editorRef.current, {
         value: value,
         language: language,
-      });
+      })});
 
       // Y.js and Monaco Binding
       const monacoBinding = new MonacoBinding(ytext, editor.getModel(), new Set([editor]), provider.awareness);
+  }
 
-      // Handle changes in the editor
-      editor.onDidChangeModelContent((e) => {
-        const newValue = editor.getValue();
-        // Update the local state
-        setValue(newValue);
-
-        // Update Y.js document to reflect local changes
-        // ytext.observe((event) => {
-        //   if (event.target === ytext) {
-        //     const yjsValue = ytext.toString();
-        //     if (newValue !== yjsValue) {
-        //       editor.setValue(yjsValue);
-        //     }
-        //   }
-        // });
-      });
-    });
-  }, [language]);
 
   return (
-    <div ref={editorRef} style={{ height: "55vh", width: "100%" }}></div>
-    // <Editor
-    //   height="55vh"
-    //   width="100%"
-    //   language={language}
-    //   value={value}
-    // />
+    // <div ref={editorRef} style={{ height: "55vh", width: "100%" }}></div>
+    
+    <Editor
+      height={"55vh"}
+      width={"100%"}
+      onMount={didEditorMount}
+      onChange={handleEditorChange}
+      language={language}
+      value={value}
+      options={options}
+    />
   );
-};
+}
 
 export default CodeEditorWindow;

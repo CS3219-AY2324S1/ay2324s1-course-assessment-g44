33cd7557d952ea86ae16bb428fa29bba4b887ca5
Sheryl Kong <sherylkong@Sheryls-MacBook-Air.local>
2023-10-25 22:18:29 +0800

    secure chatroom

--- src/components/chatbox_elements/chatbox.jsx
+++ src/components/chatbox_elements/chatbox.jsx
@@ -3,7 +3,6 @@ import {
   Card,
   Stack,
   Title,
-  Text,
   CardSection,
   ScrollArea,
   Space
@@ -12,11 +11,14 @@ import io from "socket.io-client";
 import Messages from "./messages";
 import MessageInput from "./messageinput";
 
-function Chatbox() {
+function Chatbox(props) {
   const [socket, setSocket] = useState(null);
+  const roomID = props.roomID.roomID;
 
   useEffect(() => {
     const newSocket = io(`http://${window.location.hostname}:3000`);
+    // newSocket.emit('create', props.roomID);
+    newSocket.emit('joinRoom', roomID);
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
@@ -38,10 +40,10 @@ function Chatbox() {
         {socket ? (
           <div>
             <ScrollArea h={170} type="always">
-            <Messages socket={socket} />
+            <Messages socket={socket} roomID ={roomID}/>
             </ScrollArea >
             <Space size="lg"/>
-            <MessageInput socket={socket} />
+            <MessageInput socket={socket} roomID ={roomID}/>
           </div>
         ) : (
           <div>Not Connected</div>
